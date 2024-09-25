98080ccfcc6a56605c49c147150056c2db26ba0f
DylanB <dbrum1205@gmail.com>
2024-09-24 22:37:04 -0400

    Working CreateScript

---
+++ ShopFlowPro/init_shop_flow_pro.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE IF NOT EXISTS shop_flow_pro;
+USE shop_flow_pro;
+
+-- shop_flow_status
+DROP TABLE IF EXISTS shop_flow_pro.Mechanic;
+-- Create Mechanic table
+CREATE TABLE Mechanic (
+    MID INT AUTO_INCREMENT NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    PRIMARY KEY (MID)
+);
+INSERT INTO shop_flow_pro.Mechanic (FirstName, LastName) VALUES ('Alex', 'Smith');
+INSERT INTO shop_flow_pro.Mechanic (FirstName, LastName) VALUES ('Jamie', 'Johnson');
+INSERT INTO shop_flow_pro.Mechanic (FirstName, LastName) VALUES ('Taylor', 'Brown');
+INSERT INTO shop_flow_pro.Mechanic (FirstName, LastName) VALUES ('Morgan', 'Davis');
+INSERT INTO shop_flow_pro.Mechanic (FirstName, LastName) VALUES ('Casey', 'Wilson');
+
+
+DROP TABLE IF EXISTS shop_flow_pro.Make;
+-- Create Make table
+CREATE TABLE Make (
+    MakeID INT PRIMARY KEY AUTO_INCREMENT,
+    MakeName VARCHAR(25) NOT NULL
+);
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Toyota');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Ford');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Chevy');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Honda');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Dodge');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Audi');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('BMW');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('KIA');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Nissan');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Buick');
+INSERT INTO shop_flow_pro.Make (MakeName) VALUES ('Hyundai');
+
+-- Create Mechanic-to-Make table (junction table)
+DROP TABLE IF EXISTS shop_flow_pro.MechanicToMake;
+CREATE TABLE MechanicToMake (
+    MID INT,
+    MakeID INT,
+    PRIMARY KEY (MID, MakeID),
+-- Add foreign Keys MID and MakeID
+    FOREIGN KEY (MID) REFERENCES Mechanic(MID),
+    FOREIGN KEY (MakeID) REFERENCES Make(MakeID)
+);
+DROP TABLE IF EXISTS shop_flow_pro.Vehicle;
+-- Create Vehicle table
+CREATE TABLE Vehicle (
+    VIN DECIMAL(17,0) PRIMARY KEY,
+    MakeID INT,
+    Model VARCHAR(25) NOT NULL,
+    Year INT NOT NULL,
+-- add FOREIGN KEY MakeID
+FOREIGN KEY (MakeID) REFERENCES Make(MakeID)
+);
+DROP TABLE IF EXISTS shop_flow_pro.Service;
+-- Create Service table
+CREATE TABLE Service (
+    ServiceID INT PRIMARY KEY AUTO_INCREMENT,
+    ServiceName VARCHAR(100) NOT NULL,
+    ServiceCost DECIMAL(10, 2) NOT NULL
+);
+INSERT INTO Service (ServiceName, ServiceCost) VALUES ('Tire Change', 1250);
+INSERT INTO Service (ServiceName, ServiceCost) VALUES ('Oil Change', 75.60);
+
+DROP TABLE IF EXISTS shop_flow_pro.ServiceOrder;
+-- Create ServiceOrder table
+CREATE TABLE ServiceOrder (
+    ServiceOrderID INT PRIMARY KEY AUTO_INCREMENT,
+    VIN DECIMAL(17,0),
+    MID INT,
+    ServiceID INT,
+    DateRecieved DATETIME NOT NULL,
+    DateCompleted DATETIME NULL,
+    CustomerFName VARCHAR(50),
+    CustomerLName VARCHAR(50),
+-- Create the foreign keys VIN, MID, and Service ID
+    FOREIGN KEY (VIN) REFERENCES Vehicle(VIN),
+    FOREIGN KEY (MID) REFERENCES Mechanic(MID),
+    FOREIGN KEY (ServiceID) REFERENCES Service(ServiceID)
+);
