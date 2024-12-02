3e20191b7cda5eacce7da3084720a578ca2c38fb
DylanB <dbrum1205@gmail.com>
2024-12-02 01:14:54 -0500

    Updated Component

--- ShopFlowPro-frontend/shop-flow-pro-frontend/src/app/search/search.component.ts
+++ ShopFlowPro-frontend/shop-flow-pro-frontend/src/app/search/search.component.ts
@@ -1,26 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ServiceOrderService } from '../service-order.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, NgIf, FormsModule, CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
-  @Output() searchEvent = new EventEmitter<string | undefined>();
+export class SearchComponent implements OnInit {
+  searchQuery: string = '';
+  serviceOrders: any[] = [];
+  searchResults: any[] = [];
 
-  searchForm = new FormGroup({
-    searchQuery: new FormControl('', Validators.required),
-  });
+  constructor(private serviceOrderService: ServiceOrderService) {}
+
+  ngOnInit() {
+    // Fetch the service orders from the service
+    this.serviceOrders = this.serviceOrderService.getOrders();
+    this.searchResults = this.serviceOrders; // Initially, show all orders
+  }
 
   onSearch() {
-    if (this.searchForm.valid) {
-      const searchQuery = this.searchForm.value.searchQuery || undefined; // Handle null case
-      this.searchEvent.emit(searchQuery);
+    if (this.searchQuery) {
+      this.searchResults = this.serviceOrders.filter(order =>
+        order.customer_first_name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        order.customer_last_name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        order.vin.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        order.serviceId.toString().includes(this.searchQuery)
+      );
+    } else {
+      this.searchResults = this.serviceOrders; // If no search query, show all orders
     }
   }
 }
