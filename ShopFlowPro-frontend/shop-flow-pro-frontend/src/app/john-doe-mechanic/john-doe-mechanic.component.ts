a6ce0ed981b1054abf81433211be7d81c8c78ff4
TheCodeMaster210 <119530417+TheCodeMaster210@users.noreply.github.com>
2024-12-03 22:36:49 -0500

    Added a Mechanic profile

---
+++ ShopFlowPro-frontend/shop-flow-pro-frontend/src/app/john-doe-mechanic/john-doe-mechanic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-john-doe-mechanic',
+  standalone: true,
+  imports: [],
+  templateUrl: './john-doe-mechanic.component.html',
+  styleUrl: './john-doe-mechanic.component.css'
+})
+export class JohnDoeMechanicComponent {
+
+}
